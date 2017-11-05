861e5683649035c2274c69f229282414c82163b0
Dylan Hamer <dylanhamer13@gmail.com>
2017-11-05 00:28:54 +0000

    Big lot of changes but some bugs to be worked out. Will update changelog tomorrow.

--- ZeroWM.py
+++ ZeroWM.py
@@ -5,48 +5,103 @@
  / /|  __/ | | (_) \ V  V / | |  | |
 /____\___|_|  \___/ \_/\_/  |_|  |_|
 
-Version 0.5 | Verbal Alligator
+Version 0.6 | Verbal Alligator
 
-ZeroWM 0.5
+ZeroWM 0.6
 Super simple window manager written in Python
 by Dylan Hamer
 """
 
-from Xlib import X, XK                    # X server and X keyboard
-from Xlib.display import Display          # X display
-import subprocess                         # Launch external processes
-import click                              # Handle colours and styling for log output
-from time import strftime as currentTime  # Get current time in humman readable format
+from Xlib import X, XK                      # X server and X keyboard
+from Xlib.display import Display            # X display
+from Xlib.display import colormap           # X colourmap
+import subprocess                           # Launch external processes
+import click                                # Handle colours and styling for log output
+from time import strftime as currentTime    # Get current time in humman readable format
+
+enableDebugging = False
 
 """Output log to command line"""
 def log(logLevel, logInfo):
     colourCodes = ["green", "yellow", "red", "cyan"]  # Colour codes for different error levels
     logLevels = ["[Info]    ", "[Warning] ", "[Error]   ", "[Debug]   "]  # Labels for different log levels
     timeStamp = "["+currentTime("%H:%M:%S")+"] "  # Timestamp of current time
-    click.echo(click.style(logLevels[logLevel], fg=colourCodes[logLevel])+click.style(timeStamp, fg="blue")+logInfo)  # Display error level of log event, current time and log descriptiom
+    if enableDebugging == False and logLevel == 3:
+        return
+    else:
+        click.echo(click.style(logLevels[logLevel], fg=colourCodes[logLevel])+click.style(timeStamp, fg="blue")+logInfo)  # Display error level of log event, current time and log descriptiom
+
+"""Preference class"""
+class preferences:
+    class windowManagerInfo:  # Window manager constants
+        name = "ZeroWM"
+        version = "0.6"
+        author = "Dylan Hamer"
+        releaseDate = "November 2017"
+        releaseName = "Verbal Alligator"
+        informationString = name+" "+version+": "+click.style(releaseName, fg="green")+" by "+author+" ("+releaseDate+")"+"\n"
+
+    class applicationDefaults:  # Default applications, change these to your preferred applications
+        terminal = {"name":"LXTerminal", "command":["/usr/bin/lxterminal"]}
+        launcher = {"name":"Rofi", "command":["/usr/bin/rofi", "-show", "run"]}
+        browser = {"name":"Midori", "command":["/usr/bin/midori"]}
+
+    class wallpaper:
+        wallpaperFile = "/home/dylan/Pictures/mrrobot.png"  # File of wallpaper
+        wallpaperCommand = {"name":"Feh", "command":["/usr/bin/feh", "--bg-scale", wallpaperFile]}  # Command to show wallpaper
+
+    class theme:
+        class border:
+            activeColour = "#7bfcb7"
+            inactiveColour = "#252625"
+            borderWidth = 2
 
 """Main WM class"""
 class wm(object):
     """Initialise WM variables and open display"""
     def __init__(self):
         log(0, "Starting... ")
-        self.topWindowList = []  # List of opened windows
+        self.windowList = []  # List of opened windows
         self.display = Display()  # Initialise display
+        self.colormap = self.display.screen().default_colormap  # Initialise colourmap
         self.rootWindow = self.display.screen().root  # Get root window
         self.displayWidth = self.rootWindow.get_geometry().width  # Get width of display
         self.displayHeight = self.rootWindow.get_geometry().height  # Get height of display
         self.rootWindow.change_attributes(event_mask = X.SubstructureRedirectMask)  # Redirect events from root window
         self.configureKeys()  # Configure key bindings
-                 
+
+    def mainLoop(self):
+        self.updateFocus()
+        self.updateBorders()
+        self.handleEvents()
+
     """Destroy an active window"""
-    def destroyWindow(self):
-        window = self.top_wi.pop(self.topWindowPosition)  # Remove window from list of open windows
-        window.destroy()  # Destroy the window
+    def destroyWindow(self, event):
+        self.activeWindow.destroy()
+
+    def updateFocus(self):
+        window = self.display.screen().root.query_pointer().child
+        if window != 0:
+            self.activeWindow = window
+
+    def updateBorders(self):
+        for window in self.windowList:
+            if window != self.activeWindow:
+                borderColour = preferences.theme.border.inactiveColour
+            else:
+                borderColour = preferences.theme.border.activeColour
+            borderColour = self.colormap.alloc_named_color(borderColour).pixel #borderColour).pixel
+            window.configure(border_width = preferences.theme.border.borderWidth)
+            window.change_attributes(None,border_pixel=borderColour)
+            self.display.sync()
 
     """Handle WM events"""
-    def handleEvent(self):
+    def handleEvents(self):
         ignoredEvents = [3, 33, 34, 23]  # Blacklisted events
-        event = self.display.next_event()  # Get next event from display 
+        if self.display.pending_events() > 0:
+            event = self.display.next_event()  # Get next event from display 
+        else:
+            return
         if event.type == X.MapRequest: self.handleMap(event)  # Send mapping events to the mapping handler
         elif event.type == X.KeyPress: self.handleKeyPress(event)  # Send keypress event to the keypress handler
         elif event.type in ignoredEvents: log(3, "Ignoring event: "+str(event.type))  # Ignore event if it is a blacklisted event
@@ -55,7 +110,8 @@ class wm(object):
 
     """Handle a mapping request"""
     def handleMap(self, event):
-        self.topWindowList.append(event.window)  # Add the window identifier to a list of open windows
+        self.windowList.append(event.window)  # Add the window identifier to a list of open windows
+        log(3, str(self.windowList))  # Show list of windows (DEBUG)
         self.activeWindow = event.window  # Set the active window to the mapped window
         self.activeWindowName = event.window.get_wm_name()  # Set the active window name to the window title
         event.window.map()  # Map the window
@@ -74,7 +130,8 @@ class wm(object):
         self.close = set(code for code, index in self.display.keysym_to_keycodes(XK.XK_X))
         self.t = set(code for code, index in self.display.keysym_to_keycodes(XK.XK_T))
         self.e = set(code for code, index in self.display.keysym_to_keycodes(XK.XK_E))
-        self.grabbedKeys = [self.left, self.right, self.up, self.down, self.close, self.t, self.e]
+        self.x = set(code for code, index in self.display.keysym_to_keycodes(XK.XK_X))
+        self.grabbedKeys = [self.left, self.right, self.up, self.down, self.close, self.t, self.e, self.x]
         for key in self.grabbedKeys:  # For each key to grab,
             self.grabKey(key, X.Mod1Mask)  # Grab the key with the modifer of Alt
 
@@ -106,6 +163,7 @@ class wm(object):
         elif event.detail in self.down: self.moveWindow("down")                                 # Alt+Down Arrow: move a window down
         elif event.detail in self.t: self.runProcess(preferences.applicationDefaults.terminal)  # Alt+T: Launch a terminal
         elif event.detail in self.e: self.runProcess(preferences.applicationDefaults.launcher)  # Alt+E: Launch a program launcher
+        elif event.detail in self.x: self.destroyWindow(event)                                  # ALT+X: Close a window
         else:
             log(1, "Unhandled key event!")
 
@@ -113,7 +171,7 @@ class wm(object):
     def closeDisplay(self):
         log(0, "Exiting...")
         self.display.close()
-                 
+
     """Run an application/process"""
     def runProcess(self, applicationInfo):
         try:
@@ -128,34 +186,19 @@ class wm(object):
             log(2, "Failed to launch process: "+processCommand+"!")
             log(3, str(error))
 
-class preferences:
-    class windowManagerInfo:  # Window manager constants
-        name = "ZeroWM"
-        version = "0.5"
-        author = "Dylan Hamer"
-        releaseDate = "November 2017"
-        releaseName = "Verbal Alligator"
-        informationString = name+" "+version+": "+click.style(releaseName, fg="green")+" by "+author+" ("+releaseDate+")"+"\n"
-
-    class applicationDefaults:  # Default applications, change these to your preferred applications
-        terminal = {"name":"Terminator", "command":["/usr/bin/terminator"]}
-        launcher = {"name":"Rofi", "command":["/usr/bin/rofi", "-show", "run"]}
-        browser = {"name":"Midori", "command":["/usr/bin/midori"]}
-
-    class wallpaper:
-        wallpaperFile = "/home/dylan/Pictures/mrrobot.png"  # File of wallpaper
-        wallpaperCommand = {"name":"Feh", "command":["/usr/bin/feh", "--bg-scale", wallpaperFile]}  # Command to show wallpaper
-
-def main():
+@click.option("--debug", default = False)
+def main(debug=False):
+    print debug
+    enableDebugging = debug
     print preferences.windowManagerInfo.informationString  # Display version info, author, etc at startup
     windowManager = wm()  # Initialise the WM
     windowManager.runProcess(preferences.wallpaper.wallpaperCommand)    # Set wallpaper
-    
-    while True:  
+
+    while True:
         try:
-            windowManager.handleEvent()  # Run the WM's event handler
+            windowManager.mainLoop()
         except KeyboardInterrupt:  # If CTRL-C pressed,
            windowManager.closeDisplay()  # Close the connection to the display
-           
+
 if __name__ == "__main__":
     main()
